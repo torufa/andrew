74ff558dd619eb12ec39f1b44bfb78bd2b74d8c1
torufa <torufa3@gmail.com>
2025-11-22 23:02:54 +0600

    appling in year calculation

--- js/script.js
+++ js/script.js
@@ -1,7 +1,21 @@
-let p = document.querySelector("p");
-p.style.color = "red";
-p.style.textTransform = "capitalize";
+let text = document.querySelector("#form label");
+let button = document.querySelector("#form button");
+text.classList.toggle("text");
+button.classList.toggle("text");
+button.style.borderColor = "rgb(242, 79, 79)";
+button.addEventListener("click", ()=>{button.style.borderColor = "white";});
 
-// p.classList.add("red");
-// p.classList.remove("red");
-p.classList.toggle("red");
+
+let tYear = new Date().getFullYear();
+let result = document.querySelector(".calc");
+
+result.addEventListener("click",function(e) {
+    e.preventDefault(); // Stop form reload
+
+    let bYear = Number(document.querySelector("#bdate").value);
+
+    let show = document.querySelector(".result");
+    let final = tYear-bYear;
+
+    show.innerHTML = `<i>Congratulations! You've experienced your ${final} years of life<i>`;
+})
