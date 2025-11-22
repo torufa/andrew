5d1e7157031dcfcda7721cc1061c5e0ab6170cc0
torufa <torufa3@gmail.com>
2025-11-22 20:35:25 +0600

    createElement, append, appendChild, prepend, remove, removeChild

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
-let a = document.querySelector("a");
-//a.href = "https://www.google.com";
-a.setAttribute("href", "https://www.google.com");
+let p = document.createElement("p");
+p.textContent = "its a project";
 
-let img = document.querySelector("img");
-img.setAttribute(
-    "src",
-    "https://images.unsplash.com/photo-1602922960044-d48ce791d3c0?q=80&w=1165&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
+document.querySelector("body").prepend(p);
+// document.body.append(p);
+document.body.appendChild(p);
 
-console.log(img.getAttribute("src"));
-a.removeAttribute("href");
+
+// document.querySelector("a").remove();
+let body = document.querySelector("body");
+body.removeChild(document.querySelector("a"));
