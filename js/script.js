76f4f1242abcfe0200d60699c07eef8fbe4fda65
torufa <torufa3@gmail.com>
2025-11-22 20:04:12 +0600

    set, get, removeAttribute

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
-let h1 = document.querySelector("h1");
-//h1.innerText = "22-11-2025";
-//h1.textContent = "22-11-2025!";
-h1.innerHTML = "<u>22-11-2025 - Earthquack </u>";
+let a = document.querySelector("a");
+//a.href = "https://www.google.com";
+a.setAttribute("href", "https://www.google.com");
+
+let img = document.querySelector("img");
+img.setAttribute(
+    "src",
+    "https://images.unsplash.com/photo-1602922960044-d48ce791d3c0?q=80&w=1165&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
+
+console.log(img.getAttribute("src"));
+a.removeAttribute("href");
