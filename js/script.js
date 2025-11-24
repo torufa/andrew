01c582731d44081a3d440b994a0ea8f0d0cb2728
torufa <torufa3@gmail.com>
2025-11-24 19:22:36 +0600

    length, setAttribute, removeAttribute, removeChild

--- js/script.js
+++ js/script.js
@@ -1,22 +1,10 @@
-let text = document.querySelector("#form label");
-let button = document.querySelector("#form button");
-text.classList.toggle("text");
-button.classList.toggle("text");
-button.style.borderColor = "rgb(242, 79, 79)";
-button.addEventListener("click", ()=>{button.style.borderColor = "white";});
+let text = document.querySelectorAll("i");
+console.log(text.length);
 
+let heading = document.querySelector("h2");
+heading.setAttribute("title", "Ooops! HI!")
 
-let tYear = new Date().getFullYear();
-let result = document.querySelector(".calc");
+console.log(heading.getAttribute("title"));
+heading.removeAttribute("title");
 
-result.addEventListener("click",function(e) {
-    e.preventDefault(); // Stop form reload
-
-    let bYear = Number(document.querySelector("#bdate").value);
-
-    let show = document.querySelector(".result");
-    let final = tYear-bYear;
-
-    show.innerHTML = `<i>Congratulations! You've experienced your ${final} years of life<i>`;
-});
-
+document.body.removeChild(heading);
