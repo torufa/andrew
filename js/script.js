33178efce6242538aa91c02c8713e571aee5f5ff
torufa <torufa3@gmail.com>
2025-11-25 19:35:12 +0600

    click, dblclick, remove

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
-let li = document.querySelectorAll("ul li:nth-child(2n)");
-li.forEach(function (value) {
-    value.classList.add("red")
+let h2 = document.querySelector("h2");
+h2.addEventListener("click", function(){
+    h2.style.backgroundColor = "yellow";
 })
+
+let btn = document.querySelector("button");
+btn.addEventListener("dblclick", function () {
+    btn.style.backgroundColor = "red";
+})
+
+//to remove you've to make a particular function first or might take the function at the first senario
+function dbl(){
+    btn.style.backgroundColor = "red";
+}
+//btn.addEventListener("dblclick", dbl);
+btn.removeEventListener("dblclick", dbl);
