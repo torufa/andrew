239427555fc43528cf3c18d2a40dd2d900823f12
torufa <torufa3@gmail.com>
2025-11-25 18:03:55 +0600

    adding color to particular items

--- js/script.js
+++ js/script.js
@@ -1,10 +1,4 @@
-let text = document.querySelectorAll("i");
-console.log(text.length);
-
-let heading = document.querySelector("h2");
-heading.setAttribute("title", "Ooops! HI!")
-
-console.log(heading.getAttribute("title"));
-heading.removeAttribute("title");
-
-document.body.removeChild(heading);
+let li = document.querySelectorAll("ul li:nth-child(2n)");
+li.forEach(function (value) {
+    value.classList.add("red")
+})
