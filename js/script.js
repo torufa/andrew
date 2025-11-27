b5dbdd9fa580c1d06d0b8d7affa3b7573b9f875b
torufa <torufa3@gmail.com>
2025-11-27 17:37:59 +0600

    card

--- js/script.js
+++ js/script.js
@@ -1,12 +1,35 @@
-let main = document.querySelector("#main");
-let extra = document.querySelector(".extra");
+let form = document.querySelector("form");
+let inpts = document.querySelectorAll("input");
 
-extra.addEventListener("click", function (value){
-    main.click();
-});
-main.addEventListener("change", function(value){
-    let fileName = value.target.files[0];
-    if(fileName){
-        extra.innerText = fileName.name;
-    }
-});
+let dtls = document.createElement('div');
+dtls.classList.add('dtls');
+form.appendChild(dtls);
+
+form.addEventListener("submit", function (value) {
+   value.preventDefault();
+   
+
+   let card = document.createElement("div");
+   card.classList.add("card");
+
+   let profile = document.createElement('div');
+   profile.classList.add('img');
+   let img = document.createElement('img');
+   img.setAttribute("src", inpts[0].value);
+
+   let h2 = document.createElement('h2');
+   h2.textContent = inpts[1].value;
+   let h3 = document.createElement('h3');
+   h3.textContent = inpts[2].value;
+   let p = document.createElement('p');
+   p.textContent = inpts[3].value;
+
+   profile.appendChild(img);
+   card.appendChild(profile);
+   card.appendChild(h2);
+   card.appendChild(h3);
+   card.appendChild(p);
+
+   dtls.appendChild(card);
+
+})
