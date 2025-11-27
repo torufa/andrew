283e614c2b69eefd0011d1c27049e78c326829ed
torufa <torufa3@gmail.com>
2025-11-27 22:34:03 +0600

    cursor - mousemove, mouseover, mouseout

--- js/script.js
+++ js/script.js
@@ -1,39 +1,5 @@
-let form = document.querySelector("form");
-let inpts = document.querySelectorAll("input");
-
-let dtls = document.createElement('div');
-dtls.classList.add('dtls');
-form.appendChild(dtls);
-
-form.addEventListener("submit", function (value) {
-   value.preventDefault();
-   
-
-   let card = document.createElement("div");
-   card.classList.add("card");
-
-   let profile = document.createElement('div');
-   profile.classList.add('img');
-   let img = document.createElement('img');
-   img.setAttribute("src", inpts[0].value);
-
-   let h2 = document.createElement('h2');
-   h2.textContent = inpts[1].value;
-   let h3 = document.createElement('h3');
-   h3.textContent = inpts[2].value;
-   let p = document.createElement('p');
-   p.textContent = inpts[3].value;
-
-   profile.appendChild(img);
-   card.appendChild(profile);
-   card.appendChild(h2);
-   card.appendChild(h3);
-   card.appendChild(p);
-
-   dtls.appendChild(card);
-
-   if(inpts.tytpe !== "submit"){
-      inpts.value == "";
-   }
-
+let box = document.querySelector(".box");
+window.addEventListener("mousemove", function (value) {
+   box.style.top = value.clientY + "px";
+   box.style.left = value.clientX + "px";
 })
