d9677f68d62591520ef838cc7f4a71290dbf2ff1
torufa <torufa3@gmail.com>
2025-11-30 22:17:33 +0600

    setTimeout, setInterval, clear them

--- js/script.js
+++ js/script.js
@@ -1,54 +1,19 @@
-let inps = document.querySelectorAll("input");
-let form = document.querySelector("form");
-let error = document.querySelector("error");
-let nError = document.querySelector("#nameError");
-let eError = document.querySelector("#emailError");
-let pError = document.querySelector("#passwordError");
-    let togglePass = document.querySelector("#togglePassword");
-    togglePass.addEventListener("click", (e)=>{
-        if(inps[2].type === "password"){
-            inps[2].type = "text";
-            togglePass.innerText = "hide";
-        }else{
-            inps[2].type = "password";
-            togglePass.innerText = "show"
-        }
-    })
+let bc = setInterval(() => {
+    console.log("flower");
+}, 2000);
+clearInterval(bc);
 
-form.addEventListener("submit", (e)=>{
-    e.preventDefault();
-    let valid = true;
+let ab = setTimeout(() => {
+    console.log("hello world");
+}, 1000);
 
-    //name
-    let nlen = inps[0].value.length;
-    if(nlen <= 2){
-        nError.style.display = "initial";
-        valid = false;
+
+let count = 5;
+let time = setInterval(() => {
+    if(count >= 1){
+    count--;
+    console.log(count);
     }else{
-        nError.style.display = "none";
+        clearInterval(time);
     }
-
-    //email
-    let emailRegex = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$/i;
-    if(!emailRegex.test(inps[1].value)){
-        eError.style.display = "initial";
-        valid = false;
-    }else{
-        eError.style.display = "none";
-    }
-
-    //password
-
-    const passRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{3,}$/;
-    if(!passRegex.test(inps[2].value)){
-        pError.style.display = "initial";
-        valid = false;
-    }else{
-        pError.style.display = "none";
-    }
-
-    //success msg
-    if(valid){
-        alert("successfully submited");
-    }
-});
+}, 1000);
