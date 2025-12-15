34a78980412eca4cfcc3b61a26966ed3d53deff1
torufa <torufa3@gmail.com>
2025-12-15 22:47:19 +0600

    cookie, JSON strignify and parsing

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
-sessionStorage.setItem('name', "color");
-sessionStorage.getItem('name');
-sessionStorage.removeItem('name', "color");
-sessionStorage.clear();
+document.cookie = "some text";
+document.cookie = "some = text";
+document.cookie = "hellow = hello"; //making another cookie
+
+localStorage.setItem("red", JSON.stringify({a : "dress codes", b : "yet it's a joke"}));
+let data = JSON.parse(localStorage.getItem("red"));
+console.log(data);
