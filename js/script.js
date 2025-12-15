368c979cb1663d28a0061e1c9c0fe097821cd592
torufa <torufa3@gmail.com>
2025-12-15 15:57:24 +0600

    localStorage .setItem .getItem

--- js/script.js
+++ js/script.js
@@ -1,19 +1,3 @@
-let bar = document.querySelector(".bar");
-let progress = document.querySelector(".progress");
-let btn = document.querySelector("button");
-
-btn.addEventListener("click", (e)=> {
-    e.preventDefault();
-
-
-    let count = 0;
-    let prog = setInterval(()=>{
-        if(count <= 100) {
-            count++;
-            progress.style.width = `${count}%`;
-        }else{
-            clearInterval(prog);
-        }
-    },20)
-
-})
+localStorage.setItem("one", "time passes very soon!");
+let value = localStorage.getItem("check");
+localStorage.setItem("two", "again");
